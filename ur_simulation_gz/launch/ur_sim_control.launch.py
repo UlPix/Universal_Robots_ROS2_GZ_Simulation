55c78edc3f02d959e72ce908d1353841b1a4d5dc
upix <uli.pixa@gmail.com>
2025-04-03 22:00:23 +0200

    bridge cam info topic

--- ur_simulation_gz/launch/ur_sim_control.launch.py
+++ ur_simulation_gz/launch/ur_sim_control.launch.py
@@ -206,6 +206,7 @@ def launch_setup(context, *args, **kwargs):
             "/depth_camera@sensor_msgs/msg/Image@gz.msgs.Image",
             "/rgb_camera@sensor_msgs/msg/Image@gz.msgs.Image",
             "depth_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+            "/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
             # '--ros-args',
             # '-p', 'publish_optical_frame:=true',
         ],
